2291b5fb528b027c6e5c549cb96dd8f9590197a6
rhx1138 <ryanjhendrix@gmail.com>
2021-12-14 08:49:21 -0500

    added notation to cofig

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const basePath = process.cwd();
 const { MODE } = require(`${basePath}/constants/blend_mode.js`);
 const { NETWORK } = require(`${basePath}/constants/network.js`);
 
+// need to change network to binance based
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
